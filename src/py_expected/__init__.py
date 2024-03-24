7b893a2894b645695f77b25f4bb9eb47bf1c9c31
Bjiornulf <tpeev01@gmail.com>
2024-03-24 22:38:24 +0100

    Initial Expected implementation

---
+++ src/py_expected/__init__.py
@@ -0,0 +1,1 @@
+from .expected import Expected, BadValueAccess, Unexpected
---
+++ src/py_expected/expected.py
@@ -0,0 +1,54 @@
+from typing import (
+    Callable,
+    Generic,
+    Union,
+    TypeVar,
+    Optional,
+    Any,
+)
+
+T = TypeVar('T')
+U = TypeVar('U')
+E = TypeVar('E')
+
+
+class BadValueAccess(LookupError, Generic[E]):
+    def __init__(self, error: E):
+        self._error = error
+
+
+class Unexpected(Generic[E]):
+    def __init__(self, error: Any) -> None:
+        self._error = error
+
+
+class Expected(Generic[T, E]):
+    @classmethod
+    def from_error(cls, error: E) -> 'Expected[T, E]':
+        return Expected(Unexpected(error))
+
+    def __init__(self, value_or_error: Union[T, Unexpected[E]]) -> None:
+        if isinstance(value_or_error, Unexpected):
+            self._error = value_or_error._error
+            self._has_value = False
+        else:
+            self._value = value_or_error
+            self._has_value = True
+
+    def value(self) -> T:
+        if self._has_value:
+            return self._value
+        raise BadValueAccess(self._error)
+
+    def error(self) -> E:
+        return self._error
+
+    def value_or(self, default: T) -> T:
+        if not self._has_value:
+            return default
+        return self._value
+
+    def transform(self, f: Callable[[T], U]) -> 'Expected[U, E]':
+        if self._has_value:
+            return Expected(f(self._value))
+        return Expected(Unexpected(self._error))
