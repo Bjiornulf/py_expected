c18e20a2de88ffce1fde7601894c21ca11af3f25
Bjiornulf <tpeev01@gmail.com>
2024-03-26 22:06:58 +0100

    Add missing methods

--- src/py_expected/expected.py
+++ src/py_expected/expected.py
@@ -1,15 +1,9 @@
-from typing import (
-    Callable,
-    Generic,
-    Union,
-    TypeVar,
-    Optional,
-    Any,
-)
+from typing import Any, Callable, Generic, TypeVar, Union
 
 T = TypeVar('T')
 U = TypeVar('U')
 E = TypeVar('E')
+F = TypeVar('F')
 
 
 class BadValueAccess(LookupError, Generic[E]):
@@ -46,6 +40,23 @@ class Expected(Generic[T, E]):
             self._value = value_or_error
             self._has_value = True
 
+    def __bool__(self) -> bool:
+        return self._has_value
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Expected):
+            return NotImplemented
+        if self._has_value != other._has_value:
+            return False
+
+        if self._has_value and other._has_value:
+            return self._value == other._value
+        else:
+            return self._error == other._error
+
+    def has_value(self) -> bool:
+        return bool(self)
+
     def value(self) -> T:
         if self._has_value:
             return self._value
@@ -64,4 +75,9 @@ class Expected(Generic[T, E]):
     def transform(self, f: Callable[[T], U]) -> 'Expected[U, E]':
         if self._has_value:
             return Expected(f(self._value))
-        return Expected(Unexpected(self._error))
+        return Expected.from_error(self._error)
+
+    def transform_error(self, f: Callable[[E], F]) -> 'Expected[T, F]':
+        if self._has_value:
+            return Expected(self._value)
+        return Expected.from_error(f(self._error))
